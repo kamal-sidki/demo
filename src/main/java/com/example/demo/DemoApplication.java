dadc07993393148bb5c55558ba11d2c047b79e16
kamal-sidki <kamalsidki789@gmail.com>
2022-04-23 19:04:53 +0000

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(RepoCustom customerRepository){
+		return args -> {
+			customerRepository.save(new Custom(null,"Enset","contact@enset-media.ma"));
+			customerRepository.save(new Custom(null,"FSTM","contact@fstm.ma"));
+			customerRepository.save(new Custom(null,"ENSAM","contact@ensam.ma"));
+			customerRepository.findAll().forEach(System.out::println);
+		};
+	}
+
+}
