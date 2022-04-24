07313de10dd838c342d4b3e91906a793ae39746a
kamal-sidki <kamalsidki789@gmail.com>
2022-04-24 11:14:50 +0000

    plugin for tomcat

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -18,6 +18,7 @@ public class DemoApplication {
 			customerRepository.save(new Custom(null,"Enset","contact@enset-media.ma"));
 			customerRepository.save(new Custom(null,"FSTM","contact@fstm.ma"));
 			customerRepository.save(new Custom(null,"ENSAM","contact@ensam.ma"));
+			customerRepository.save(new Custom(null,"ENSAM2","contact@ensam.ma"));
 			customerRepository.findAll().forEach(System.out::println);
 		};
 	}
